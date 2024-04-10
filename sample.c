2a015154aea5e32af6ae092c9a3cfe910f333e81
Yasumasa Tamura <ytamura@ist.hokudai.ac.jp>
2024-04-10 10:55:23 +0900

    add sample program

---
+++ sample.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+
+#include <limits.h> // `UINT32_MAX` を使用するために必要
+
+#include "mt19937.h"
+
+int main(void) {
+    // シード値を実行のたびに変える
+    seed((uint_fast32_t)time(NULL));
+
+    // 例 1) [0, 100] の整数乱数を10回生成
+    printf("Integer [0, 100]\n");
+    printf("----------------\n");
+    for (int i = 0; i < 10; ++i) {
+        printf("%u\n", genrand() % 100);
+    }
+    printf("\n");
+
+    // 例 2) [0.0, 1.0] の浮動小数点数を10回生成
+    printf("Floating point [0.0, 1.0]\n");
+    printf("-------------------------\n");
+    for (int i = 0; i < 10; ++i) {
+        float rnd = genrand() / (float)UINT32_MAX;
+        printf("%f\n", rnd);
+    }
+
+    return 0;
+}
